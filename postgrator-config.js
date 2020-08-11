cf0709e7b3987dd9fc91cac48f707b5e24f38466
sarawarnock <sara@sarawarnock.com>
2020-08-11 14:11:36 -0600

    live database url

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
-  "host": process.env.MIGRATION_DB_HOST,
-  "port": process.env.MIGRATION_DB_PORT,
-  "database": process.env.MIGRATION_DB_NAME,
-  "username": process.env.MIGRATION_DB_USER,
-  "password": process.env.MIGRATION_DB_PASS
+  "host": process.env.LIVE_MIGRATION_DB_HOST,
+  "port": process.env.LIVE_MIGRATION_DB_PORT,
+  "database": process.env.LIVE_MIGRATION_DB_NAME,
+  "username": process.env.LIVE_MIGRATION_DB_USER,
+  "password": process.env.LIVE_MIGRATION_DB_PASS
 }
