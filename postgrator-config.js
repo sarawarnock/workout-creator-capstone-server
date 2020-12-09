123fcb5c863da61cf90fffe9d9a533905201ec50
Bowie <bowie@thinkful.com>
2020-12-09 08:06:58 -0800

    fixed psql / changed migration exports / changed db_name for .env

--- postgrator-config.js
+++ postgrator-config.js
@@ -9,3 +9,14 @@ module.exports = {
   "username": process.env.LIVE_MIGRATION_DB_USER,
   "password": process.env.LIVE_MIGRATION_DB_PASS
 }
+
+console.log('migrate to NODE_ENV::', process.env.DB_URL)
+
+module.exports = {
+  "migrationsDirectory": "migrations",
+  "driver": "pg",
+  "connectionString": (process.env.NODE_ENV === 'test')
+      ? process.env.TEST_DATABASE_URL
+      : process.env.DB_URL,
+  "ssl": !!process.env.SSL,
+};
