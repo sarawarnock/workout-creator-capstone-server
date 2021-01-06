2ea6e69187cf46611f498b235d9d0e29f51378e2
sarawarnock <sara@sarawarnock.com>
2021-01-06 11:36:32 -0700

    more cors

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(morgan(morganOption, {
 
 app.use(cors())
 const corsOptions = {
-  origin: 'https://workout-creator-server.herokuapp.com/api',
+  origin: 'https://workout-creator-capstone-client.vercel.app/',
   optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions));
@@ -46,5 +46,3 @@ app.use('/api/workoutdetails', workoutDetailsRouter)
 app.use(errorHandler)
 
 module.exports = app
-
-// test
